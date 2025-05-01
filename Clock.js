78b6c89da8671cc2e6d35015f278a8053357880f
Mushtaque Ali <mushtaqueali.bscsf23@iba-suk.edu.pk>
2025-05-01 02:47:32 -0700

    Add files via upload

---
+++ Clock.js
@@ -0,0 +1,18 @@
+function updateClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    let meridium = hours >= 12 ? "PM" : "AM";
+    hours = hours%12 || 12;
+    hours = hours.toString().padStart(2,0)
+    const min = now.getMinutes().toString().padStart(2,0);
+    const sec = now.getSeconds().toString().padStart(2,0);
+
+    const timeString = `${hours}:${min}:${sec}:${meridium}`;
+
+    document.getElementById("clock").textContent = timeString;
+
+}
+
+
+updateClock();
+setInterval(updateClock, 1000);
